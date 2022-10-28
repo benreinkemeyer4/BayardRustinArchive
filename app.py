10a9c5f9efb40d4e0e5075f8a9948ffdcdc2dd10
Kathleen Li <kxli@princeton.edu>
2022-10-28 10:43:40 -0400

    changed photos

--- app.py
+++ app.py
@@ -8,17 +8,21 @@ app = flask.Flask(__name__, template_folder='./templates')
 
 
 @app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
+@app.route('/index', methods=['GET', 'POST'])
 def index(): #has upload media function
+    if flask.request.method == 'POST':
+        print("UPLOAD CLICKED", file=sys.stdout)
+        app.logger.info('UPLOAD CLICKED')
+
     html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
     return response
 
-@app.route('/', methods=['POST'])
-def upload_file():
-    print("UPLOAD CLICKED", file=sys.stdout)
-    app.logger.info('UPLOAD CLICKED')
-    #request.form['customFileInput']
+# @app.route('/', methods=['POST'])
+# def upload_file():
+#     print("UPLOAD CLICKED", file=sys.stdout)
+#     app.logger.info('UPLOAD CLICKED')
+#     #request.form['customFileInput']
 
 @app.route('/submit', methods=['GET'])
 def submit_form(): #form to fill out additional info
