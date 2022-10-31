e8a9be66386210119eaeb4b8c397a88d3af96682
Alison Lee <alison@dynamic-oit-vapornet100-10-9-89-22.princeton.edu>
2022-10-30 23:28:44 -0400

    gallery

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, make_response, redirect, flash
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
+from database.query_db import query_db
 import os
 
 
@@ -84,3 +85,11 @@ def thank_you():
     response = make_response(html_code)
     return response
 
+@app.route('/gallery', methods=['GET'])
+def gallery():
+    results = query_db()
+    print(results)
+    html_code = render_template('gallery.html', \
+        results = results)
+    response = make_response(html_code)
+    return response
--- database/query_db.py
+++ database/query_db.py
@@ -4,29 +4,31 @@ import urllib.parse as up
 
 url = up.urlparse("postgres://oegsfiae:WUg1B4yX8l8PXcVH_E87mjkgD6IfcTOV@peanut.db.elephantsql.com/oegsfiae")
 
-try:
-    up.uses_netloc.append("postgres")
 
-    # Connect to an existing database
-    connection = psycopg2.connect(database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port)
+def query_db(): 
+    try:
+        up.uses_netloc.append("postgres")
 
-    # Create a cursor to perform database operations
-    cursor = connection.cursor()
-    # Executing a SQL query
-    cursor.execute("SELECT * FROM public.submissions LIMIT 100;")
-    # Fetch result
-    record = cursor.fetchall()
-    print(record)
+        # Connect to an existing database
+        connection = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port)
 
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+        # Executing a SQL query
+        cursor.execute("SELECT * FROM public.submissions LIMIT 100;")
+        # Fetch result
+        record = cursor.fetchall()
+        return record
 
-finally:
-    if (connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
